f7b7b17935e430dd9a2ffd3d6977291909b6131a
rpolicastro <rpolicas@indiana.edu>
2019-05-30 13:50:09 -0400

    adding peak calling for the sampled bams

--- main.sh
+++ main.sh
@@ -6,6 +6,9 @@ source settings.conf
 ## RIF Like Analysis
 #####################
 
+## Preparing Run
+## -------------
+
 ## activating environment
 
 source active chip-downsampling
@@ -14,21 +17,50 @@ source active chip-downsampling
 
 source settings.conf
 
-## downsampling BAM
+## Downsampling BAM
+## ----------------
 
-# get total number of reads in BAM
+##  get total number of reads in BAM
+
 READS=$(samtools flagstat $BAM | awk 'NR==1' | cut -d" " -f1)
 
-# divide number by 2 if paired
+## divide number by 2 if paired
+
 [[ $PAIRED = TRUE ]] && READS=$(python -c "print $READS/2")
 
-# get sampling percentage
+## get sampling percentage
+
 SAMPLE_FRAC=$(python -c "print float($SAMPLE)/float($READS)")
 
-# sample BAM
-mkdir -p ${WORKDIR}/results
+## sample BAM
+
+mkdir -p ${WORKDIR}/results/sampled_bams
 
 samtools view -bs 0$SAMPLE_FRAC $BAM | \
-samtools sort -O BAM -o ${WORKDIR}/results/${SAMPLE}_sampled_$(basename $BAM)
+samtools sort -O BAM -o ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM)
 
-samtools index ${WORKDIR}/results/${SAMPLE}_sampled_$(basename $BAM)
+samtools index ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM)
+
+## Peak Calling
+## ------------
+
+mkdir -p ${WORKDIR}/results/sampled_peaks
+
+if [ $PAIRED = TRUE ]
+then
+	macs2 callpeak \
+	-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
+	-c $CONTROL \
+	-f BAMPE \
+	-g $GENOME_SIZE \
+	--outdir ${WORKDIR}/results/sampled_peaks \
+	-n ${SAMPLE}_sampled_$(basename $BAM .bam)
+else
+	macs2 callpeak \
+	-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
+	-c $CONTROL \
+	-f BAM \
+	-g $GENOME_SIZE \
+	--outdir ${WORKDIR}/results/sampled_peaks \
+	-n ${SAMPLE}_sampled_$(basename $BAM .bam)
+fi
