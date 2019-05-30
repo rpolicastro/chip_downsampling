68e0579820d99a5d55636f2472a321014ca7b6e3
rpolicastro <rpolicas@indiana.edu>
2019-05-30 11:43:17 -0400

    creating main file

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source settings.conf
+
+#####################
+## RIF Like Analysis
+#####################
+
+## activating environment
+
+source active chip-downsampling
+
+## loading settings
+
+source settings.conf
+
+## downsampling BAM
+
+
