94c1aa34776c061430b0eebf7c7e965c4786ae70
rpolicastro <rpolicas@indiana.edu>
2019-05-30 13:29:28 -0400

    finished BAM sampling

--- main.sh
+++ main.sh
@@ -16,4 +16,19 @@ source settings.conf
 
 ## downsampling BAM
 
+# get total number of reads in BAM
 READS=$(samtools flagstat $BAM | awk 'NR==1' | cut -d" " -f1)
+
+# divide number by 2 if paired
+[[ $PAIRED = TRUE ]] && READS=$(python -c "print $READS/2")
+
+# get sampling percentage
+SAMPLE_FRAC=$(python -c "print float($SAMPLE)/float($READS)")
+
+# sample BAM
+mkdir -p ${WORKDIR}/results
+
+samtools view -bs 0$SAMPLE_FRAC $BAM | \
+samtools sort -O BAM -o ${WORKDIR}/results/${SAMPLE}_sampled_$(basename $BAM)
+
+samtools index ${WORKDIR}/results/${SAMPLE}_sampled_$(basename $BAM)
