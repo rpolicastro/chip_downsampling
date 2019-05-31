6c9a70670724641dfce7a85319a33f8dd0829ad8
rpolicastro <rpolicas@indiana.edu>
2019-05-31 09:24:11 -0400

    setting range of sampled values

--- main.sh
+++ main.sh
@@ -26,42 +26,52 @@ READS=$(samtools flagstat $BAM | awk 'NR==1' | cut -d" " -f1)
 
 [[ $PAIRED = TRUE ]] && READS=$(python -c "print $READS/2")
 
-## get sampling percentage
+## get sampling percentages
 
-SAMPLE_FRAC=$(python -c "print float($SAMPLE)/float($READS)")
+SAMPLES=($(seq $FROM $BY $TO))
+
+SAMPLE_FRACS=()
+for SAMPLE in ${SAMPLES[@]}; do
+	SAMPLE_FRACS+=($(python -c "print float($SAMPLE)/float($READS)"))
+done
 
 ## sample BAM
 
 mkdir -p ${WORKDIR}/results/sampled_bams
 
-samtools view -bs 0$SAMPLE_FRAC $BAM | \
-samtools sort -O BAM -o ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM)
+N_SAMPLES=${#SAMPLE_FRACS[@]}
+for N in $(seq 0 1 $(bc <<< $N_SAMPLES-1)); do
+	samtools view -bs 0${SAMPLE_FRACS[$N]} $BAM | \
+	samtools sort -O BAM -o ${WORKDIR}/results/sampled_bams/${SAMPLES[$N]}_sampled_$(basename $BAM)
 
-samtools index ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM)
+	samtools index ${WORKDIR}/results/sampled_bams/${SAMPLES[$N]}_sampled_$(basename $BAM)
+done
 
 ## Peak Calling
 ## ------------
 
 mkdir -p ${WORKDIR}/results/sampled_peaks
 
-if [ $PAIRED = TRUE ]
-then
-	macs2 callpeak \
-	-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
-	-c $CONTROL \
-	-f BAMPE \
-	-g $GENOME_SIZE \
-	--outdir ${WORKDIR}/results/sampled_peaks \
-	-n ${SAMPLE}_sampled_$(basename $BAM .bam)
-else
-	macs2 callpeak \
-	-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
-	-c $CONTROL \
-	-f BAM \
-	-g $GENOME_SIZE \
-	--outdir ${WORKDIR}/results/sampled_peaks \
-	-n ${SAMPLE}_sampled_$(basename $BAM .bam)
-fi
+for SAMPLE in ${SAMPLES[@]}; do
+	if [ $PAIRED = TRUE ]
+	then
+		macs2 callpeak \
+		-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
+		-c $CONTROL \
+		-f BAMPE \
+		-g $GENOME_SIZE \
+		--outdir ${WORKDIR}/results/sampled_peaks \
+		-n ${SAMPLE}_sampled_$(basename $BAM .bam)
+	else
+		macs2 callpeak \
+		-t ${WORKDIR}/results/sampled_bams/${SAMPLE}_sampled_$(basename $BAM) \
+		-c $CONTROL \
+		-f BAM \
+		-g $GENOME_SIZE \
+		--outdir ${WORKDIR}/results/sampled_peaks \
+		-n ${SAMPLE}_sampled_$(basename $BAM .bam)
+	fi
+done
 
 ## Annotating BAM fragments against Peaks
 ## --------------------------------------
